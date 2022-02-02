390304cdf1049531f1ac363b60b1fcb0fa0ef417
agoodney <andrew.goodney@gmail.com>
2022-02-02 13:55:18 -0800

    better resistor template

--- scripts/Templates.py
+++ scripts/Templates.py
@@ -7,14 +7,14 @@ def Resistor(id):
     Property("Footprint", "", Id(2), PositionIdentifier(0,0), TextEffects(Font(1.27, 1.27), hide=True)),
     Property("Datasheet", "", Id(3), PositionIdentifier(0, 8.89, 0), TextEffects(Font(1.27, 1.27), hide=True))]
 
-    pts = PointsList([XY(-2.54,0), XY(-1.905,0.635), XY(-1.27,-0.635), XY(-0.635,0.635), XY(0,-0.635), XY(0.635,0.635), XY(1.27,-0.635), XY(1.905,0)])
+    pts = PointsList([XY(-3.302,0), XY(-3.048,0), XY(-3.048,0), XY(-2.54,0.762), XY(-2.032,-0.762), XY(-1.524,0.762), XY(-1.016,-0.762), XY(-0.508,0.762), XY(0,-0.762), XY(0.508,0),XY(0.508,0),XY(0.762,0)])
     stroke = Stroke(width=0, type='default', r=0, g=0, b=0, a=0)
     fill = Fill(type='none')
     line = SymbolLine(pts, stroke, fill)
     g = GrUnit(library_id=id, unit=0, style=1, elements=[line])
 
-    pin1 = Pin('passive', 'line', PositionIdentifier(-4.445, 0, 0), 1.905, PinName("~", TextEffects(Font(1.27, 1.27))), PinNumber("1",TextEffects(Font(1.27, 1.27))))
-    pin2 = Pin('passive', 'line', PositionIdentifier(3.81, 0, 180), 1.905, PinName("~", TextEffects(Font(1.27, 1.27))), PinNumber("2",TextEffects(Font(1.27, 1.27))))
+    pin1 = Pin('passive', 'line', PositionIdentifier(-5.08,0,0), 1.905, PinName("~", TextEffects(Font(1.27, 1.27))), PinNumber("1",TextEffects(Font(1.27, 1.27))))
+    pin2 = Pin('passive', 'line', PositionIdentifier(2.54,0,180), 1.905, PinName("~", TextEffects(Font(1.27, 1.27))), PinNumber("2",TextEffects(Font(1.27, 1.27))))
 
     pins = PinUnit(library_id=id, unit=1, style=1, elements=[pin1, pin2])
 
